e9e19dc692475d52b583386e74d32cd20c840560
Drew Gallagher <drewg2009@gmail.com>
2016-10-22 16:43:48 -0400

    changed to new CORS plugin

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,6 +15,8 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+        'Barryvdh\Cors\HandleCors'
+
     ];
 
     /**
@@ -49,7 +51,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Foundation\Http\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'cors' => \App\Http\Middleware\Cors::class, // <<< add this line
 
     ];
 }
