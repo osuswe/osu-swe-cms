c914210d07e805a6f0347bd75bcbfdb62926bb0f
Drew Gallagher <drewg2009@gmail.com>
2016-11-23 01:20:08 -0500

    added date formatting inputs. changed date formatting in logic. notifications and current time tested to be correct for now.

--- resources/views/admin/events/create.blade.php
+++ resources/views/admin/events/create.blade.php
@@ -18,7 +18,7 @@
         <div class="form-group {{ $errors->has('date') ? 'has-error' : ''}}">
             {!! Form::label('date', 'Date', ['class' => 'col-sm-3 control-label']) !!}
             <div class="col-sm-6">
-                {!! Form::text('date', null, ['class' => 'form-control', 'required' => 'required', 'id'=>'date']) !!}
+                {!! Form::date('date', null, ['class' => 'form-control', 'required' => 'required', 'id'=>'date']) !!}
                 {!! $errors->first('date', '<p class="help-block">:message</p>') !!}
             </div>
         </div>
@@ -36,20 +36,28 @@
                 {!! $errors->first('description', '<p class="help-block">:message</p>') !!}
             </div>
         </div>
-        <div class="form-group {{ $errors->has('time_range') ? 'has-error' : ''}}">
-            {!! Form::label('time_range', 'Time Range', ['class' => 'col-sm-3 control-label']) !!}
-            <div class="col-sm-6">
-                {!! Form::text('time_range', null, ['class' => 'form-control', 'required' => 'required', 'id'=>'time_range']) !!}
-                {!! $errors->first('time_range', '<p class="help-block">:message</p>') !!}
-            </div>
-        </div>
-        <div class="form-group {{ $errors->has('time_range') ? 'has-error' : ''}}">
+
+        <div class="form-group {{ $errors->has('event_code') ? 'has-error' : ''}}">
             {!! Form::label('event_code', 'Event Code', ['class' => 'col-sm-3 control-label']) !!}
             <div class="col-sm-6">
                 {!! Form::text('event_code', null, ['class' => 'form-control','id'=>'event_code','required' => 'required',]) !!}
                 {!! $errors->first('event_code', '<p class="help-block">:message</p>') !!}
             </div>
         </div>
+        <div class="form-group">
+            {!! Form::label('start_time', 'Start Time', ['class' => 'col-sm-3 control-label']) !!}
+            <div class="col-sm-6">
+                {!! Form::time('start_time', null, ['class' => 'form-control', 'required' => 'required', 'id'=>'start_time']) !!}
+            </div>
+        </div>
+        <div class="form-group">
+            {!! Form::label('end_time', 'End Time', ['class' => 'col-sm-3 control-label']) !!}
+            <div class="col-sm-6">
+                {!! Form::time('end_time', null, ['class' => 'form-control', 'required' => 'required', 'id'=>'end_time']) !!}
+            </div>
+        </div>
+
+        <input type="hidden" id="time_range" name="time_range" required />
         <input type="hidden" id="notification_send_time" name="notification_send_time" value="" required/>
 
         <div class="form-group">
@@ -79,36 +87,71 @@
 
             var eventDate = document.getElementById("date").value;
             console.log(eventDate);
-            var eventTime = document.getElementById("time_range").value;
-            console.log(eventTime);
-
+            // var eventTime = document.getElementById("time_range").value;
+            // console.log(eventTime);
 
             var twentyFourHours = 86400000;
 
-            var eventDateSplit = eventDate.split("/");
-            var month = eventDateSplit[0] - 1;//months start at 0 index
-            var day = eventDateSplit[1];
-            var year = eventDateSplit[2];
+            //format of date is yyyy-mm-dd
+            var eventDateSplit = eventDate.split("-");
+            var year = eventDateSplit[0];
+            var month = eventDateSplit[1] - 1;//months start at 0 index
+            var day = eventDateSplit[2];
+
 
             //console.log("Time value: " + this.time);
 
-            var timeSplit = eventTime.split("-");
+            var startTime = document.getElementById('start_time').value;
+            var endTime = document.getElementById('end_time').value;
 
-            var startTime = timeSplit[0];
+            console.log(startTime);
             var startHours = Number(startTime.match(/^(\d+)/)[1]);
             var startMinutes = Number(startTime.match(/:(\d+)/)[1]);
-            var am_pm_split = startTime.split(" ");
-            var AMPM = am_pm_split[1];
-            console.log("Start am/pm: " + AMPM);
-            if (AMPM.toLowerCase() == "pm" && startHours < 12) startHours = startHours + 12;
-            if (AMPM.toLowerCase() == "am" && startHours == 12) startHours = startHours - 12;
+
+            var endHours = Number(endTime.match(/^(\d+)/)[1]);
+            var endMinutes = Number(endTime.match(/:(\d+)/)[1]);
+
+            // console.log(startHours);
+            // console.log(startMinutes);
+            var startAMPM="";
+            var endAMPM="";
+
+            //figure out am/pm for start time
+            if(startHours >= 12){
+              startAMPM="pm";
+            }
+            else{
+              startAMPM = "am";
+            }
+
+            //figure out am/pm for end time
+            if(endHours >= 12){
+              endAMPM="pm";
+            }
+            else{
+              endAMPM = "am";
+            }
 
             var eventDateObj = new Date(year, month, day, startHours, startMinutes, 0, 0);
 
             //set notification delivery time to 24 hours before event
             eventDateObj.setTime(eventDateObj.getTime() - twentyFourHours);
+
             //put date in a OneSignal acceptable format
             document.getElementById("notification_send_time").value=eventDateObj.toString();
+
+            console.log("Start am/pm: " + startAMPM);
+
+            //reformat time_range to be displayable
+            if (startAMPM.toLowerCase() == "pm" && startHours >= 12) startHours = startHours - 12;
+            // if (startAMPM.toLowerCase() == "am" && startHours == 12) startHours = startHours - 12;
+
+            if (endAMPM.toLowerCase() == "pm" && endHours >= 12) endHours = endHours - 12;
+            // if (endAMPM.toLowerCase() == "am" && endHours == 12) endHours = endHours - 12;
+
+
+            //build time_range from start_time and end_time fields
+            document.getElementById('time_range').value=startHours+":"+startMinutes+" "+startAMPM+"-"+endHours+":"+endMinutes+" " + endAMPM;
 
             document.getElementById('form').submit();
 
