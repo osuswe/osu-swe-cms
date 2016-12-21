3176b163d39ce7a4b7217a9b9126a1880e9c56c3
Drew Gallagher <drewg2009@gmail.com>
2016-12-20 21:13:20 -0500

    now initializing major box value

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -56,36 +56,36 @@
         </div>
         <div class="form-group {{ $errors->has('major') ? 'has-error' : ''}}">
             {!! Form::label('major', 'Major', ['class' => 'col-sm-3 control-label']) !!}
-                <div class="col-sm-6">
-                    <select id="selectMajor">
-                        <option value="Aeronautical and Astronautical Engineering" selected>Aeronautical and
-                            Astronautical Engineering
-                        </option>
-                        <option value="Architecture">Architecture</option>
-                        <option value="Aviation">Aviation</option>
-                        <option value="Biomedical Engineering">Biomedical Engineering</option>
-                        <option value="Chemical Engineering">Chemical Engineering</option>
-                        <option value="Civil Engineering">Civil Engineering</option>
-                        <option value="Computer Science and Engineering">Computer Science and Engineering</option>
-                        <option value="Electrical and Computer Engineering">Electrical and Computer Engineering</option>
-                        <option value="Engineering Physics">Engineering Physics</option>
-                        <option value="Environmental Engineering"> Environmental Engineering</option>
-                        <option value="Faculty/staff"> Faculty/staff</option>
-                        <option value="Food, Agricultural and Biological Engineering"> Food, Agricultural and Biological
-                            Engineering
-                        </option>
-                        <option value="Industrial and Systems Engineering">Industrial and Systems Engineering</option>
-                        <option value="Materials Science and Engineering">Materials Science and Engineering</option>
-                        <option value="Mechanical Engineering"> Mechanical Engineering</option>
-                        <option value="Non-Engineering"> Non-Engineering</option>
-                        <option value="Undecided Engineering">Undecided Engineering</option>
-                        <option value="Welding Engineering">Welding Engineering</option>
-                    </select>
+            <div class="col-sm-6">
+                <select id="selectMajor">
+                    <option value="Aeronautical and Astronautical Engineering" selected>Aeronautical and
+                        Astronautical Engineering
+                    </option>
+                    <option value="Architecture">Architecture</option>
+                    <option value="Aviation">Aviation</option>
+                    <option value="Biomedical Engineering">Biomedical Engineering</option>
+                    <option value="Chemical Engineering">Chemical Engineering</option>
+                    <option value="Civil Engineering">Civil Engineering</option>
+                    <option value="Computer Science and Engineering">Computer Science and Engineering</option>
+                    <option value="Electrical and Computer Engineering">Electrical and Computer Engineering</option>
+                    <option value="Engineering Physics">Engineering Physics</option>
+                    <option value="Environmental Engineering"> Environmental Engineering</option>
+                    <option value="Faculty/staff"> Faculty/staff</option>
+                    <option value="Food, Agricultural and Biological Engineering"> Food, Agricultural and Biological
+                        Engineering
+                    </option>
+                    <option value="Industrial and Systems Engineering">Industrial and Systems Engineering</option>
+                    <option value="Materials Science and Engineering">Materials Science and Engineering</option>
+                    <option value="Mechanical Engineering"> Mechanical Engineering</option>
+                    <option value="Non-Engineering"> Non-Engineering</option>
+                    <option value="Undecided Engineering">Undecided Engineering</option>
+                    <option value="Welding Engineering">Welding Engineering</option>
+                </select>
 
-            <br><br>
-                    {!! Form::text('major', null, ['class' => 'form-control', 'required' => 'required', 'id'=>'majorBox']) !!}
-                    {!! $errors->first('major', '<p class="help-block">:message</p>') !!}
-                </div>
+                <br><br>
+                {!! Form::text('major', null, ['class' => 'form-control', 'required' => 'required', 'id'=>'majorBox']) !!}
+                {!! $errors->first('major', '<p class="help-block">:message</p>') !!}
+            </div>
         </div>
 
 
@@ -123,6 +123,10 @@
             passwordBox.value = "osu-swe";
 
             var majorBox = document.getElementById('majorBox');
+
+            //init major box value
+            majorBox.value = selectMajor.options[selectMajor.selectedIndex].value;
+
             //majorBox=selectMajor.options[selectMajor.selectedIndex].value;
             selectMajor.onchange = function () {
                 majorBox.value = selectMajor.options[selectMajor.selectedIndex].value;
