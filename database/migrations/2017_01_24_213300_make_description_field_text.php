23b3183c9a7a99ccda86f329ba2fc34569b52461
Drew Gallagher <drewg2009@gmail.com>
2017-01-24 16:43:25 -0500

    Removed unneeded migration. Wrote migration to extend description field to text data type

--- database/migrations/2017_01_24_213300_make_description_field_text.php
+++ database/migrations/2017_01_24_213300_make_description_field_text.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class MakeDescriptionFieldText extends Migration
 {
     /**
      * Run the migrations.
@@ -12,10 +12,8 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token')->index();
-            $table->timestamp('created_at');
+        Schema::table('events', function (Blueprint $table) {
+            $table->text("description")->change();
         });
     }
 
@@ -26,6 +24,9 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::drop('password_resets');
+        Schema::table('events', function (Blueprint $table) {
+            $table->string("description")->change();
+
+        });
     }
 }
